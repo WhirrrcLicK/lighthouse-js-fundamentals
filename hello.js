1802b69795debcf5e96fbae3e104cd1865af53b3
Kaiya Hedley <keshedley@gmail.com>
2022-07-11 14:28:02 -0600

    updated hello and adding even

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
 const sayHello = function (name) {
-  console.log("Hello, " + name);
+  return "Hello, " + name;
 }
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+const greeting = sayHello("John")
+console.log(greeting);
